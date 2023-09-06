7e222ee9a55ba4cbcdaa7c860ec77f622f954f44
SIP <SIP@bizzappdev.com>
2023-09-06 15:59:39 +0530

    [T6419]connector_woo_partner: removed with_company.

--- connector_woo_partner/models/woo_backend/common.py
+++ connector_woo_partner/models/woo_backend/common.py
@@ -14,7 +14,7 @@ class WooBackend(models.Model):
         filters = {"page": 1}
         for backend in self:
             filters.update({"per_page": backend.default_limit})
-            backend.env["woo.res.partner"].with_company().with_delay(
+            backend.env["woo.res.partner"].with_delay(
                 priority=5
             ).import_batch(backend=backend, filters=filters)
 
