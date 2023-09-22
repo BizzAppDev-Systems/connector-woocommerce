772a64dfc6a75585e8661a6f1fb3bdf6255143e6
PAA <PAA@bizzappdev.com>
2023-09-22 19:35:04 +0530

    [WIP]connector_woocommerce:added test cases

---
+++ connector_woocommerce/tests/test_import_product.py
@@ -0,0 +1,63 @@
+from os.path import dirname, join
+
+from vcr import VCR
+from .test_woo_backend import BaseWooTestCase
+
+recorder = VCR(
+    cassette_library_dir=join(dirname(__file__), "fixtures/cassettes"),
+    decode_compressed_response=True,
+    filter_headers=["Authorization"],
+    path_transformer=VCR.ensure_suffix(".yaml"),
+    record_mode="once",
+)
+
+
+class TestImportProduct(BaseWooTestCase):
+    def setUp(self):
+        """Setup configuration for Product."""
+        super().setUp()
+
+    def test_import_product_product(self):
+        """Test Assertions for Product"""
+        external_id = "50"
+        with recorder.use_cassette("import_woo_product_product"):
+            self.env["woo.product.product"].import_record(
+                external_id=external_id, backend=self.backend
+            )
+        self.product_model = self.env["woo.product.product"]
+        product1 = self.product_model.search([("external_id", "=", external_id)])
+        self.assertEqual(len(product1), 1)
+        self.assertTrue(product1, "Woo Product is not imported!")
+        self.assertEqual(
+            product1.external_id, external_id, "External ID is different!!"
+        )
+        self.assertEqual(
+            product1.name,
+            "Shirt",
+            "Product's name is not matched with response!",
+        )
+        self.assertEqual(
+            product1.purchase_ok,
+            False,
+            "Purchase is not matched with response!",
+        )
+        self.assertEqual(
+            product1.list_price,
+            500.0,
+            "List Price is not matched with response",
+        )
+        self.assertEqual(
+            product1.default_code,
+            "shirt-sku",
+            "default_code is not match with response",
+        )
+        self.assertEqual(
+            product1.price,
+            "500",
+            "Price is not matched with response",
+        )
+        self.assertEqual(
+            product1.description,
+            "<p>shirt.</p>\n",
+            "description is not matched with response",
+        )
