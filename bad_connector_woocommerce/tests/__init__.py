534462c67cb42c4ed5debadaa3a22203fb899b19
SIP <SIP@bizzappdev.com>
2023-10-12 19:20:22 +0530

    [T6756]bad_connector_woocommerce: Added test case for countries and state.

--- bad_connector_woocommerce/tests/__init__.py
+++ bad_connector_woocommerce/tests/__init__.py
@@ -6,3 +6,4 @@ from . import test_import_product_attribute
 from . import test_import_product
 from . import test_import_sale_order
 from . import test_import_woo_tax
+from . import test_import_country_and_state
