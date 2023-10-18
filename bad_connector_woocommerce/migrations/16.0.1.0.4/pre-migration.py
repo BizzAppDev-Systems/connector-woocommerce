1b8127381f014a3c8ddd7674674f2ed0f0267dd0
SIP <SIP@bizzappdev.com>
2023-10-18 19:33:21 +0530

    [T6842]bad_connector_woocommerce:Improved migration.

--- bad_connector_woocommerce/migrations/16.0.1.0.4/pre-migration.py
+++ bad_connector_woocommerce/migrations/16.0.1.0.4/pre-migration.py
@@ -1,25 +1,13 @@
+from odoo import SUPERUSER_ID, api
+
+
 def migrate(cr, version):
-    cr.execute(
-        """
-        ALTER TABLE sale_order
-        ADD COLUMN IF NOT EXISTS woo_order_status_id INT;
-        """
-    )
-    cr.execute(
-        """
-        UPDATE sale_order
-        SET woo_order_status_id =
-            CASE
-                WHEN woo_order_status = 'pending' THEN 1
-                WHEN woo_order_status = 'processing' THEN 2
-                WHEN woo_order_status = 'on-hold' THEN 3
-                WHEN woo_order_status = 'completed' THEN 4
-                WHEN woo_order_status = 'cancelled' THEN 5
-                WHEN woo_order_status = 'refunded' THEN 6
-                WHEN woo_order_status = 'failed' THEN 7
-                WHEN woo_order_status = 'trash' THEN 8
-                WHEN woo_order_status = 'auto-draft' THEN 9
-                ELSE 0
-            END;
-        """
-    )
+    env = api.Environment(cr, SUPERUSER_ID, {})
+    sale_orders = env["sale.order"].search([])
+
+    for order in sale_orders:
+        woo_order_status_id = env["woo.sale.status"].search(
+            [("code", "=", order.woo_order_status)], limit=1
+        )
+        if woo_order_status_id:
+            order.write({"woo_order_status_id": woo_order_status_id.id})
