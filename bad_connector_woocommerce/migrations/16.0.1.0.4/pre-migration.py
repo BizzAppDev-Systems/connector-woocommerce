005435ce233f923df719b13cc33d31e78c28364c
SIP <SIP@bizzappdev.com>
2023-10-19 12:46:39 +0530

    [T6842]bad_connector_woocommerce:Improve migration by adding where condition

--- bad_connector_woocommerce/migrations/16.0.1.0.4/pre-migration.py
+++ bad_connector_woocommerce/migrations/16.0.1.0.4/pre-migration.py
@@ -1,22 +1,26 @@
 def migrate(cr, version):
-    # Check if the column 'woo_order_status' exists in the table
     cr.execute(
         """
-        SELECT * FROM information_schema.columns
-        WHERE table_name = 'sale_order'
-        AND column_name = 'woo_order_status'
+        ALTER TABLE sale_order
+        ADD COLUMN IF NOT EXISTS woo_order_status_id INT;
         """
     )
-    column_exists = cr.fetchone()
-
-    # Add the column 'woo_order_status_id' if it doesn't exist
-    if not column_exists:
-        cr.execute(
-            "ALTER TABLE sale_order ADD COLUMN IF NOT EXISTS woo_order_status_id INT"
-        )
-
-    # Update the 'woo_order_status_id' based on 'woo_order_status' values
     cr.execute(
-        "UPDATE sale_order SET woo_order_status_id = %s WHERE woo_order_status = %s",
-        (4, "completed"),
+        """
+        UPDATE sale_order
+        SET woo_order_status_id =
+            CASE
+                WHEN woo_order_status = 'pending' THEN 1
+                WHEN woo_order_status = 'processing' THEN 2
+                WHEN woo_order_status = 'on-hold' THEN 3
+                WHEN woo_order_status = 'completed' THEN 4
+                WHEN woo_order_status = 'cancelled' THEN 5
+                WHEN woo_order_status = 'refunded' THEN 6
+                WHEN woo_order_status = 'failed' THEN 7
+                WHEN woo_order_status = 'trash' THEN 8
+                WHEN woo_order_status = 'auto-draft' THEN 9
+                ELSE 0
+            END;
+        WHERE woo_bind_ids = True;
+        """
     )
