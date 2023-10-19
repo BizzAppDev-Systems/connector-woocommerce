86934ccaa29427a790340906847579e534a53c17
SIP <SIP@bizzappdev.com>
2023-10-19 11:50:55 +0530

    [T6842]bad_connector_woocommerce:Improve migration.

--- bad_connector_woocommerce/migrations/16.0.1.0.4/pre-migration.py
+++ bad_connector_woocommerce/migrations/16.0.1.0.4/pre-migration.py
@@ -1,14 +1,22 @@
 def migrate(cr, version):
+    # Check if the column 'woo_order_status' exists in the table
     cr.execute(
         """
-        ALTER TABLE sale_order
-        ADD COLUMN IF NOT EXISTS woo_order_status_id INT
+        SELECT * FROM information_schema.columns
+        WHERE table_name = 'sale_order'
+        AND column_name = 'woo_order_status'
         """
     )
+    column_exists = cr.fetchone()
+
+    # Add the column 'woo_order_status_id' if it doesn't exist
+    if not column_exists:
+        cr.execute(
+            "ALTER TABLE sale_order ADD COLUMN IF NOT EXISTS woo_order_status_id INT"
+        )
+
+    # Update the 'woo_order_status_id' based on 'woo_order_status' values
     cr.execute(
-        """
-        UPDATE sale_order
-        SET woo_order_status_id = 4
-        WHERE woo_order_status = 'completed'
-        """
+        "UPDATE sale_order SET woo_order_status_id = %s WHERE woo_order_status = %s",
+        (4, "completed"),
     )
