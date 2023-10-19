c08a6b4edaa9d82c5a105df568defe7e880df92a
SIP <SIP@bizzappdev.com>
2023-10-19 10:29:53 +0530

    [T6842]bad_connector_woocommerce:Improved migration

--- bad_connector_woocommerce/migrations/16.0.1.0.4/pre-migration.py
+++ bad_connector_woocommerce/migrations/16.0.1.0.4/pre-migration.py
@@ -1,13 +1,14 @@
-from odoo import SUPERUSER_ID, api
-
-
 def migrate(cr, version):
-    env = api.Environment(cr, SUPERUSER_ID, {})
-    sale_orders = env["sale.order"].search([])
-
-    for order in sale_orders:
-        woo_order_status_id = env["woo.sale.status"].search(
-            [("code", "=", order.woo_order_status)], limit=1
-        )
-        if woo_order_status_id:
-            order.write({"woo_order_status_id": woo_order_status_id.id})
+    cr.execute(
+        """
+        ALTER TABLE sale_order
+        ADD COLUMN IF NOT EXISTS woo_order_status_id INT
+        """
+    )
+    cr.execute(
+        """
+        UPDATE sale_order
+        SET woo_order_status_id = 1
+        WHERE woo_order_status = 'pending'
+        """
+    )
