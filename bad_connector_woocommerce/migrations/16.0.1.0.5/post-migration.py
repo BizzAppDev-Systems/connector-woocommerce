0ec8a4c3de678dc26faa23ae89b03143c5678200
SIP <SIP@bizzappdev.com>
2023-10-20 11:27:26 +0530

    [T6830]bad_connector_woocommerce:Improved the Migration script also change the version of folder.

--- bad_connector_woocommerce/migrations/16.0.1.0.5/post-migration.py
+++ bad_connector_woocommerce/migrations/16.0.1.0.5/post-migration.py
@@ -14,7 +14,12 @@ def migrate(cr, version):
     cr.execute(
         """
         ALTER TABLE sale_order
-        ADD COLUMN IF NOT EXISTS woo_order_status_id INT;
+        ADD COLUMN IF NOT EXISTS woo_order_status_id INT
+        """
+    )
+    cr.execute(
+        """
+        ALTER TABLE sale_order
         ADD CONSTRAINT fk_woo_order_status_id
         FOREIGN KEY (woo_order_status_id)
         REFERENCES woo_sale_status(id);
