c22ae097744c404cbbf828525a681aba83456cce
SIP <SIP@bizzappdev.com>
2023-12-14 14:16:08 +0530

    [T6930]grouped_product_bad_connector_woocommerce: Added module of grouped_product_bad_connector_woocommerce.

---
+++ grouped_product_bad_connector_woocommerce/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name": "Grouped Product Odoo Woocommerce Connector",
+    "version": "16.0.3.0.0",
+    "category": "Connector",
+    "author": "BizzAppDev Systems Pvt. Ltd.",
+    "website": "http://www.bizzappdev.com",
+    "depends": [
+        "bad_connector_woocommerce",
+        "mrp",
+    ],
+    "images": ["static/description/banner.gif"],
+    "license": "AGPL-3",
+}
