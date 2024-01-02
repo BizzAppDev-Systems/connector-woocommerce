74420f1a8f39313ba0ab490083cf22451cb854cc
SIP <SIP@bizzappdev.com>
2024-01-02 15:30:00 +0530

    [T7148]bad_connector_woocommerce:Improved the code.

--- bad_connector_woocommerce/models/stock_picking/exporter.py
+++ bad_connector_woocommerce/models/stock_picking/exporter.py
@@ -19,76 +19,52 @@ class WooStockPickingRefundExporterMapper(Component):
         """Mapping for Quantity"""
         line_items = []
         total_amount = 0.00
-        divided_tax = 0
         sale_order_products = {
             order_line.product_id.id for order_line in record.sale_id.order_line
         }
         for move in record.move_ids:
             if move.product_id.id in sale_order_products:
                 order_line = next(
-                    order_line
-                    for order_line in record.sale_id.order_line
-                    if order_line.product_id.id == move.product_id.id
+                    (
+                        order_line
+                        for order_line in record.sale_id.order_line
+                        if order_line.product_id.id == move.product_id.id
+                    ),
+                    None,
                 )
-                if move.quantity_done > order_line.woo_bind_ids[0].product_uom_qty:
+                if not order_line:
+                    continue
+
+                quantity_done = move.quantity_done
+                price_unit = float(order_line.woo_bind_ids[0].price_unit)
+                total_tax_line = float(order_line.woo_bind_ids[0].total_tax_line)
+
+                if quantity_done > order_line.woo_bind_ids[0].product_uom_qty:
                     raise MappingError(
                         _(
-                            "Quantity done of move line is greater than quantity in WooCommerce Product Quantity."
+                            "Quantity done of move line is greater than quantity in "
+                            "WooCommerce Product Quantity."
                         )
                     )
-                if order_line.woo_bind_ids.product_uom_qty > move.quantity_done:
-                    divided_tax = (
-                        float(order_line.woo_bind_ids[0].total_tax_line)
-                        / order_line.woo_bind_ids[0].product_uom_qty
-                    )
-                    list_item = {
-                        "id": order_line.woo_bind_ids[0].external_id,
-                        "quantity": move.quantity_done,
-                        "refund_total": float(order_line.woo_bind_ids[0].price_unit)
-                        * move.quantity_done,
-                        "refund_tax": [
-                            {
-                                "id": str(move.id),
-                                "refund_total": divided_tax * move.quantity_done,
-                            }
-                        ],
-                    }
-                    total_amount = (
-                        total_amount
-                        + (
-                            float(order_line.woo_bind_ids[0].price_unit)
-                            * move.quantity_done
-                        )
-                        + (divided_tax * move.quantity_done)
-                    )
-                else:
-                    list_item = {
-                        "id": order_line.woo_bind_ids[0].external_id,
-                        "quantity": move.quantity_done,
-                        "refund_total": float(order_line.woo_bind_ids[0].price_unit)
-                        * move.quantity_done,
-                        "refund_tax": [
-                            {
-                                "id": str(move.id),
-                                "refund_total": float(
-                                    order_line.woo_bind_ids[0].total_tax_line
-                                )
-                                * move.quantity_done,
-                            }
-                        ],
-                    }
-                    total_amount = (
-                        total_amount
-                        + (
-                            float(order_line.woo_bind_ids[0].price_unit)
-                            * move.quantity_done
-                        )
-                        + (
-                            float(order_line.woo_bind_ids[0].total_tax_line)
-                            * move.quantity_done
-                        )
-                    )
+
+                divided_tax = (
+                    total_tax_line / order_line.woo_bind_ids[0].product_uom_qty
+                )
+
+                list_item = {
+                    "id": order_line.woo_bind_ids[0].external_id,
+                    "quantity": quantity_done,
+                    "refund_total": price_unit * quantity_done,
+                    "refund_tax": [
+                        {
+                            "id": str(move.id),
+                            "refund_total": divided_tax * quantity_done,
+                        }
+                    ],
+                }
+                total_amount += (price_unit + divided_tax) * quantity_done
                 line_items.append(list_item)
+
         return {
             "order_id": record.sale_id.woo_bind_ids[0].external_id,
             "amount": str(total_amount),
