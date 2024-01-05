156cc9c357c0b872bac88cc449d70d08edf2efa3
SIP <SIP@bizzappdev.com>
2024-01-05 11:11:10 +0530

    [T7136]bad_connector_woocommerce:Added the logger.

--- bad_connector_woocommerce/controllers/main.py
+++ bad_connector_woocommerce/controllers/main.py
@@ -41,9 +41,14 @@ class WooWebhook(http.Controller):
             )
             raise Forbidden()
         payload = json.loads(request.httprequest.data)
+        payload_status = payload.get("status")
         if model_name == "woo.sale.order":
             status = backend.woo_sale_status_ids.mapped("code")
-            if status and payload.get("status") not in status:
+            if status and payload_status not in status:
+                _logger.info(
+                    "Skipping sale order import due to status: %s",
+                    payload_status,
+                )
                 return True
         model = request.env[model_name]
         description = backend.get_queue_job_description(
