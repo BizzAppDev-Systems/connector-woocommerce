6d82df189089849e6242bcbf8afc63656e82a3f1
SIP <SIP@bizzappdev.com>
2024-02-27 10:51:31 +0530

    [T7365]bad_connector_woocommerce:Rename the external_move.

--- bad_connector_woocommerce/models/stock_move/common.py
+++ bad_connector_woocommerce/models/stock_move/common.py
@@ -4,4 +4,4 @@ from odoo import fields, models
 class StockMove(models.Model):
     _inherit = "stock.move"
 
-    external_move = fields.Char(string="WooCommerce External ID for stock move")
+    external_move = fields.Char(string="WooCommerce External ID for Stock Move")
