476027f0d5aced78edaae1b7d0a587638ccd01f8
SIP <SIP@bizzappdev.com>
2024-03-07 13:11:29 +0530

    [T7520]bad_connector_woocommerce:Added the code of support multiwarehouse.

--- bad_connector_woocommerce/models/product/common.py
+++ bad_connector_woocommerce/models/product/common.py
@@ -153,6 +153,32 @@ class WooProductProduct(models.Model):
             self._recompute_woo_qty_backend(backend, woo_products)
         return True
 
+    # def _recompute_woo_qty_backend(self, backend, products, read_fields=None):
+    #     """
+    #     Recompute the products quantity for one backend.
+
+    #     If field names are passed in ``read_fields`` (as a list), they
+    #     will be read in the product that is used in
+    #     :meth:`~._woo_qty`.
+
+    #     """
+    #     if backend.product_stock_field_id:
+    #         stock_field = backend.product_stock_field_id.name
+    #     else:
+    #         stock_field = "virtual_available"
+    #     location = backend.warehouse_id.lot_stock_id
+    #     product_fields = ["woo_product_qty", stock_field]
+    #     if read_fields:
+    #         product_fields += read_fields
+    #     self_with_location = self.sudo().with_context(location=location.id)
+    #     for chunk_ids in utils.chunks(products.ids, backend.recompute_qty_step):
+    #         records = self_with_location.browse(chunk_ids)
+    #         for product in records:
+    #             new_qty = self._woo_qty(product, backend, location, stock_field)
+    #             if not new_qty != product["woo_product_qty"]:
+    #                 continue
+    #             product.woo_product_qty = new_qty
+
     def _recompute_woo_qty_backend(self, backend, products, read_fields=None):
         """
         Recompute the products quantity for one backend.
@@ -166,18 +192,21 @@ class WooProductProduct(models.Model):
             stock_field = backend.product_stock_field_id.name
         else:
             stock_field = "virtual_available"
-        location = backend.warehouse_id.lot_stock_id
+
         product_fields = ["woo_product_qty", stock_field]
         if read_fields:
             product_fields += read_fields
-        self_with_location = self.sudo().with_context(location=location.id)
-        for chunk_ids in utils.chunks(products.ids, backend.recompute_qty_step):
-            records = self_with_location.browse(chunk_ids)
-            for product in records:
-                new_qty = self._woo_qty(product, backend, location, stock_field)
-                if not new_qty != product["woo_product_qty"]:
-                    continue
-                product.woo_product_qty = new_qty
+
+        warehouses = backend.warehouse_ids
+        for warehouse in warehouses:
+            location = warehouse.lot_stock_id
+            self_with_location = self.sudo().with_context(location=location.id)
+            for chunk_ids in utils.chunks(products.ids, backend.recompute_qty_step):
+                records = self_with_location.browse(chunk_ids)
+                for product in records:
+                    new_qty = self._woo_qty(product, backend, location, stock_field)
+                    if new_qty != product["woo_product_qty"]:
+                        product.woo_product_qty = new_qty
 
     def _woo_qty(self, product, backend, location, stock_field):
         """
