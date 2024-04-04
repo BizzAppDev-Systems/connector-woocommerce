78867d9234be590601ed1619e1d09f78e6f0e145
SIP <SIP@bizzappdev.com>
2024-04-04 18:44:09 +0530

    [T7403]bad_connector_woocommerce:Added the validation back.

--- bad_connector_woocommerce/models/stock_picking/importer.py
+++ bad_connector_woocommerce/models/stock_picking/importer.py
@@ -264,8 +264,15 @@ class WooStockPickingRefundImporter(Component):
                     lambda a: a.backend_id == self.backend_record
                 )
                 ext_id = int(woo_product_id.external_id)
-                if ext_id not in product_line_mapping:
+                if (
+                    move.quantity_done == move.product_uom_qty
+                    and ext_id not in product_line_mapping
+                ):
                     continue
+                elif ext_id not in product_line_mapping:
+                    raise ValidationError(
+                        _("External ID not found of Product: %s" % move.product_id.name)
+                    )
                 move.external_move = product_line_mapping[ext_id]
                 move.quantity_done = move.product_uom_qty
             if not self.backend_record.process_return_automatically:
