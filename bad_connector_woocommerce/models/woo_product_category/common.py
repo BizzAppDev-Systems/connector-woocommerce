bce863ed6193002f27f0b46cc6758f7f63e972dd
PAA <PAA@bizzappdev.com>
2024-04-12 11:57:42 +0530

    [ADD][T7271]bad_connector_woocommerce:Added product category dependency

--- bad_connector_woocommerce/models/woo_product_category/common.py
+++ bad_connector_woocommerce/models/woo_product_category/common.py
@@ -50,3 +50,9 @@ class WooProductCategoryAdapter(Component):
     _apply_on = "woo.product.category"
     _woo_model = "products/categories"
     _woo_ext_id_key = "id"
+    _model_dependencies = {
+        (
+            "woo.product.category",
+            "parent_id",
+        )
+    }
