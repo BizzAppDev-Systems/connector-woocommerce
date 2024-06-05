a1ed905f1e0ca4cd2be5c9a6e00ef3f101b97f28
RUS <RUS@bizzappdev.com>
2024-06-05 19:43:00 +0530

    [RELEASE] 17.0.1.0.7

--- bad_connector_woocommerce/__manifest__.py
+++ bad_connector_woocommerce/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Odoo Woocommerce Connector",
-    "version": "17.0.1.0.6",
+    "version": "17.0.1.0.7",
     "category": "Connector",
     "author": "BizzAppDev Systems Pvt. Ltd.",
     "website": "http://www.bizzappdev.com",
