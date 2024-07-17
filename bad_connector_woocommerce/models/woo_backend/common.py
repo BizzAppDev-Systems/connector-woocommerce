690e647a182586b0cb7b13aa1f2ad924b95aca20
DHR <dhr@bizzappdev.com>
2024-07-17 18:39:37 +0530

    [IMP][16.0][T8144] bad_connector_woocommerce: get back changes regarding setup

--- bad_connector_woocommerce/models/woo_backend/common.py
+++ bad_connector_woocommerce/models/woo_backend/common.py
@@ -198,6 +198,12 @@ class WooBackend(models.Model):
         processed and validated automatically.""",
     )
     activity_user_id = fields.Many2one("res.users", string="Responsible User")
+    # Added new field
+    map_product_based_on_sku = fields.Boolean(
+        help="""If this boolean is set to True, the system will update products that
+        have a matching SKU with the default code. Please enable this option if you
+        want to update products with matching SKU during the import process.""",
+    )
 
     @api.depends("test_mode", "test_access_token", "access_token")
     def _compute_webhook_config(self):
