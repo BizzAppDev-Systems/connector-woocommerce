e9a8de5b8ae4c8b090295a31cc5e8535a3dd22ed
KHP <KHP@bizzappdev.com>
2025-04-09 17:51:14 +0530

    [MIG] [T8414] grouped_product_bad_connector_woocommerce: Migration to 18.0

--- grouped_product_bad_connector_woocommerce/__manifest__.py
+++ grouped_product_bad_connector_woocommerce/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Grouped Product Odoo Woocommerce Connector",
-    "version": "17.0.1.0.10",
+    "version": "18.0.1.0.0",
     "category": "Connector",
     "author": "BizzAppDev Systems Pvt. Ltd.",
     "website": "http://www.bizzappdev.com",
